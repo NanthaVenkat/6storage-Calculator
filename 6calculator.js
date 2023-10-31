6c328908de637afc46d560fd4d49352f5a29f225
NanthaKumar <120775708+NanthaVenkat@users.noreply.github.com>
2023-10-31 16:50:49 +0530

    Add files via upload

---
+++ 6calculator.js
@@ -0,0 +1,6 @@
+const plansSold = document.getElementById("plans-sold");
+const monReven = document.getElementById("mon-reven");
+const monProfit = document.getElementById("mon-profit");
+const annualProfit = document.getElementById("annual-profit");
+const markCap = document.getElementById("mark-cap");
+
